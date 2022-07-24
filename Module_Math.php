a4a8b30a8c4c8160dfd97d96899713a2d8d379a2
gizmore <gizmore@wechall.net>
2022-07-24 22:12:48 +0200

    '7.0.1-r1401'

--- Module_Math.php
+++ Module_Math.php
@@ -3,7 +3,24 @@ namespace GDO\Math;
 
 use GDO\Core\GDO_Module;
 
+/**
+ * Math evaluator module.
+ * 
+ * @author gizmore
+ * @version 7.0.1
+ * @since 7.0.0
+ */
 final class Module_Math extends GDO_Module
 {
+	public int $priority = 20;
+	
+	public function checkSystemDependencies() : bool
+	{
+		if (!function_exists('bcadd'))
+		{
+			return $this->errorSystemDependency('err_php_extension_missing', ['bcmath']);
+		}
+		return true;
+	}
 	
 }
